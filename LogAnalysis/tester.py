d2687de89beb45dd5db3afadc4519568d7a5974e
nzscode <93976904+nzscode@users.noreply.github.com>
2023-06-26 01:31:23 -0400

    Update tester.py

--- LogAnalysis/tester.py
+++ LogAnalysis/tester.py
@@ -1,21 +1,6 @@
-basket = [("apples", 5), ("cantaloupe", 9), ("dates", 3), ("apples", 10), ("pears", 8)]
 fruits = {
-    "apples": 2,
-    "oranges": 5,
-    "pears": 3
+    "Bananas": 3,
+    "Olives": 9,
 }
 
-# for fruit in fruits:
-#     fruits[fruit] += 1
-#
-# print(fruits)
-
-fruit_list = {}
-for i in range(len(basket)):
-    basket_item = basket[i][0]
-    if basket_item in fruits:
-        fruits[basket_item] += int(basket[i][1])
-    elif basket_item not in fruits:
-        fruits[basket_item] = int(basket[i][1])
-
-print(fruits)
+print(fruits.keys())
