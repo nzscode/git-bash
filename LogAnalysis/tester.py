3cfacd5302424d75d70e3d999377a44b8deb93af
nzscode <93976904+nzscode@users.noreply.github.com>
2023-06-26 16:13:51 -0400

    Update tester.py

--- LogAnalysis/tester.py
+++ LogAnalysis/tester.py
@@ -1,6 +1,21 @@
+import operator
 fruits = {
-    "Bananas": 3,
-    "Olives": 9,
+    "Tangerines": [12, "orange"],
+    "Bananas": [3, "yellow"],
+    "Olives": [9, "black"],
+    "Grapes": [5, "purple"],
+    "Kiwi": [2, "green"],
+    "Avocado": [8, "green"],
 }
+sorted_fruits = sorted(fruits.items(), key=operator.itemgetter(0), reverse=True)
+# print(sorted_fruits)
+fruits_list = []
+# for fruit in fruits:
+#     fruits_list.append([fruit, fruits[fruit][0], fruits[fruit][1]])
 
-print(fruits.keys())
+# fruits_list.sort(key=lambda a: a[2], reverse=True)
+for fruit in sorted_fruits:
+    fruits_list.append([fruit[0], fruit[1][0], fruit[1][1]])
+fruits_list.insert(0, ["Fruits", "Count", "Color"])
+print(fruits_list)
+# print(sorted_fruits[0][0])
