a3d15e15a36a46cea31bdc9367df65e2270d9beb
Noor Zaqir <nzscode@gmail.com>
2023-07-22 09:59:18 -0400

    Update print_name.py

--- Week2/scripts/print_name.py
+++ Week2/scripts/print_name.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+
 def printName(name, age):
     print(f"My name is {name}, I am {age} years old.")
     if age < 13:
         print("I am a pre-teen.")
-    elif age > 13 and age < 20:
+    elif age >= 13 and age < 20:
         print ("I am a teen.")
     else:
         print("I am an adult now.")
 
-printName("Ibrahim", 17)
+printName("Zaqir", 43)
