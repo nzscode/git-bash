a2131eee2feb52e6a8f39540c0ecf253d782549e
Noor Zaqir <nzscode@gmail.com>
2023-07-24 19:56:48 -0400

    Create newBranchTestScript.py

---
+++ Week2/scripts/newBranchTestScript.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+  print("This is a new test file, in the 'Even Better Branch'")
+<<<<<<< HEAD
+  print("This is a new edit")
+=======
+  print("Thsi is a change in the even-better-branch")
+>>>>>>> even-better-branch
+main()
