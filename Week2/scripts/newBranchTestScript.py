d6cb1510e90e3db37a5ba62211f26c70b0e37987
Noor Zaqir <nzscode@gmail.com>
2023-07-25 19:23:32 -0400

    Update newBranchTestScript.py

--- Week2/scripts/newBranchTestScript.py
+++ Week2/scripts/newBranchTestScript.py
@@ -2,9 +2,5 @@
 
 def main():
   print("This is a new test file, in the 'Even Better Branch'")
-<<<<<<< HEAD
-  print("This is a new edit")
-=======
   print("Thsi is a change in the even-better-branch")
->>>>>>> even-better-branch
 main()
