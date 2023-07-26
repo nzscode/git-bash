9a3cbfa87c9548b583976d7b7f14d1b281823d0d
Noor Zaqir <nzscode@gmail.com>
2023-07-26 10:01:36 -0400

    Set up the main brainch main.py for pets and if statements

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 print("Hello")
-Q1 = input("How old are you?")
-print(f"You are {Q1} years old")
+pet = ""
+age = int(input("How old are you?  "))
+if age > 30:
+  print("Your ideal pet is a cat.")
+elif age < 30 and age > 14:
+  Q_Pet = input("What is your ideal pet?  ")
+  pet = Q_Pet
+  print(f"Your ideal pet is a {pet}")
+else:
+  pet = "fish" 
+  print(f"The pet for you is a {pet}")
+
