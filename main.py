452e4a5d42f36112d4c4a13adb0bb5b48f592c68
Noor Zaqir <nzscode@gmail.com>
2023-08-15 10:47:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
 #!/usr/bin/env python3
-print("Hello")
-pet = ""
-age = int(input("How old are you?  "))
-if age > 30:
-  print("Your ideal pet is a cat.")
-elif age < 30 and age > 14:
-  Q_Pet = input("What is your ideal pet?  ")
-  pet = Q_Pet
-  print(f"Your ideal pet is a {pet}")
-else:
-  pet = "fish"
-  print(f"The pet for you is a {pet}")
-
+for i in range(5):
+    print(i)
