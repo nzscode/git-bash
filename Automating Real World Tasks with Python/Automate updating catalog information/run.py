7de898164f0fa7954704f083ba9c9dbbb03fe075
Noor Zaqir <nzscode@gmail.com>
2023-09-09 08:33:31 -0400

    Update run.py

--- Automating Real World Tasks with Python/Automate updating catalog information/run.py
+++ Automating Real World Tasks with Python/Automate updating catalog information/run.py
@@ -1,24 +1,23 @@
-import os, sys
-import json
+#!/usr/bin/env python3
+import os
+
 import requests
 
 path = "supplier-data/descriptions/"
-descriptions = os.listdir(path)
 url = "http://localhost/fruits/"
 
+files = os.listdir(path)
 
-
-for file in descriptions:
+for file in files:
     if file.endswith("txt"):
-        with open(path + file, 'r') as text_file:
+        with open(path + file, 'r') as f:
+            # grab the file name, ex. 001, 002 to use for image file
             fruit_name = os.path.splitext(file)[0]
-            # print(fruit_name)
-            data = text_file.read()
+            data = f.read()
             data = data.split("\n")
-            test_dict = {"name": data[0], "weight": int(data[1].strip( "lbs")), "description": data[2], "image_name": fruit_name + ".jpeg"}
-            # print(test_dict)
-
-            response = requests.post(url, json=test_dict)
-
-
-
+            fruit_dic = {"name": data[0], "weight": int(data[1].strip(" lbs")), "description": data[2],
+                         "image_name": fruit_name + ".jpeg"}
+            response = requests.post(url, json=fruit_dic)
+            response.raise_for_status()
+            print(response.request.url)
+            print(response.status_code)
